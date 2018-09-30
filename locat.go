eb52fc2812aeb9888bd2e60c40ffe7a6581ebbb1
Mark Rovetta <mkrnrt@gmail.com>
2018-09-30 09:51:23 -0700

    read and unmarshall geojson file

--- locat.go
+++ locat.go
@@ -1,21 +1,17 @@
 package main
 
-
 import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"reflect"
 )
-
 
 // Sites struct which contains
 // an array of features
 type Sites struct {
 	Sites []Site `json:"features"`
 }
-
 
 // Site struct which contains a name
 // a type and a list of geometry links
@@ -25,7 +21,6 @@ type Site struct {
 	Param Param `json:"properties"`
 }
 
-
 // Geometry struct which contains a
 // list of links
 type Geometry struct {
@@ -33,39 +28,27 @@ type Geometry struct {
 	Coordinates []float32 `json:"coordinates"` 
 }
 
-
 // Param struct which contains a
 // list of links
 type Param struct {	
-	Congd string `json:"congressional_district"`
 	Statel string `json:"location_state"`
-	Webs string `json:"website"`
 	City string `json:"city"`
 	Zipcode string `json:"zipcode"`
 	Loczip string `json:"location_zip"`
 	Cupsid string `json:"cleanup_site_id"`
 	Latitude string `json:"latitude"`
-	Hec string `json:"has_environmental_covenant"`
 	County string `json:"county_name"`
-	Rank string `json:"rank"`
 	Locadd string `json:"location_address"`
-	Ressec string `json:"responsible_section"`
 	Cupname string `json:"cleanup_site_name"`
-	Nfad string `json:"no_further_action_date"`
 	Loccity string `json:"location_city"`
 	Stat string `json:"statute"`
 	Longitude string `json:"longitude"`
-	Legd string `json:"legislative_district"`
-	Webdesc string `json:"website_description"`
-	Nfar string `json:"no_further_action_reason"`
 	Cupstatus string `json:"cleanup_status"`
-	Region string `json:"region"`
 	Address string `json:"address"`
 	Facid string `json:"facility_site_id"`	
 }
 
 func main() {
-
 
 	// Open our jsonFile
 	jsonFile, err := os.Open("response.json")
@@ -74,19 +57,15 @@ func main() {
 		fmt.Println(err)
 	}
 
-
 	fmt.Println("Successfully Opened response.json")
 	// defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
 
-
 	// read our opened xmlFile as a byte array.
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-
 	// we initialize our Sites array
 	var features Sites
-
 
 	// we unmarshal our byteArray which contains our
 	// jsonFile's content into 'features' which we defined above
@@ -96,17 +75,20 @@ func main() {
 	// print out the user Type, their name, and their facebook url
 	// as just an example
 	for i := 0; i < len(features.Sites); i++ {
+	  	fmt.Println("\n")	
 		fmt.Println("Type: " + features.Sites[i].Type)
 		fmt.Println("City: " + features.Sites[i].Param.City)
 		fmt.Println("Zipcode: " + features.Sites[i].Param.Zipcode)
 		fmt.Println("Longitude: " + features.Sites[i].Param.Longitude)
 		fmt.Println("Latitude: " + features.Sites[i].Param.Latitude)
-		fmt.Println(reflect.TypeOf(features.Sites[i].Geometry.Coordinates).String())		
+		fmt.Println("Status: " + features.Sites[i].Param.Cupstatus)
+		fmt.Println("Site ID: " + features.Sites[i].Param.Cupsid)
+		fmt.Println("Facility ID: " + features.Sites[i].Param.Facid)
+		fmt.Println("Site Name: " + features.Sites[i].Param.Cupname)
+		fmt.Println("Address: " + features.Sites[i].Param.Address)		
 	  	fmt.Printf("lon: %f \n",features.Sites[i].Geometry.Coordinates[0])
 	  	fmt.Printf("lat: %f \n",features.Sites[i].Geometry.Coordinates[1])
-	  	fmt.Println("\n")		
 	}
-
-
+	  	fmt.Println("\n")	
 }
 
